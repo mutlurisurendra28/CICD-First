d01b88ddb24a506b123c31d8d2121e2b23aa9f01
CharanReddy2607 <madhucharanreddy246@gmail.com>
2025-07-29 11:21:54 +0530

    version-2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to the CICD Class</h1>
+      <p>This is section S211.</p>
+    </div>
+  )
+}
+
+export default App
